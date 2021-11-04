3ed450c39d34642ec5a5961530673d874e44587e
Samuel Hernandez <samdavid2000@hotmail.com>
2021-11-04 17:28:12 -0400

    Progress by Samuel 11/4/2021

--- Biodiversity/Biodiversity/Views/ConservationPage.xaml.cs
+++ Biodiversity/Biodiversity/Views/ConservationPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,12 @@ namespace Biodiversity.Views
         public ConservationPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            
         }
     }
 }
