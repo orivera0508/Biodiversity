2b26e8fcbd8594699e3f91e96bc48a23eedac209
Orlando Rivera Guevara <rivera_104943@students.pupr.edu>
2021-11-04 22:14:54 -0400

    Added Models: Item, Question and Answer. Fixed the NuGet Package and rerouted to sqlit-net-pcl

---
+++ Biodiversity/Biodiversity/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biodiversity.Models
+{
+    class Question
+    {
+        //questions(questionID:int, questionString:String, correctAnswerID:int)
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string question { get; set; }
+
+        public int correct_answer_Id { get; set; }
+
+    }
+}
