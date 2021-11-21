cea5177d5086ec8fa5b5a1cb6a2c88f87d0a94bc
Orlando Rivera Guevara <rivera_104943@students.pupr.edu>
2021-11-21 00:18:53 -0500

    Added Firebase Compatability

---
+++ Biodiversity/Biodiversity/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biodiversity.Models
+{
+    class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Lat { get; set; }
+
+        public float Lng { get; set; }
+
+        public string Type { get; set; }
+
+        public string Desc { get; set; }
+
+        //public string Trivia { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
---
+++ Biodiversity/Biodiversity/Services/DBFirebase.cs
@@ -0,0 +1,46 @@
+﻿using Biodiversity.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biodiversity.Services
+{
+    class DBFirebase
+    {
+        FirebaseClient client;
+        private int id;
+        private string name;
+        private float lat;
+        private float lng;
+        private string desc;
+        private string imageName;
+        private string imagePath;
+
+        public DBFirebase()
+        {
+            client = new FirebaseClient("https://biodiversity-b2d4d-default-rtdb.firebaseio.com/");
+        }
+
+        public ObservableCollection<Animal> getAnimal()
+        {
+            var animalData = client
+                .Child("Animals")
+                .AsObservable<Animal>()
+                .AsObservableCollection();
+            
+            return animalData;
+        }
+
+        public async Task AddAnimal(int Id, string Name, float Lat, float Lng, string Type, string Desc, string ImageName, string ImagePath)
+        {
+            Animal a = new Animal() { Id = id, Name = name, Lat = lat, Lng = lng, Type = Type, Desc = desc, ImageName = imageName, ImagePath = imagePath };
+            await client
+                .Child("Animals")
+                .PostAsync(a);
+        }
+    }
+}
