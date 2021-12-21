309fbfe573668d8804bcd10861e80614bbe197d0
Samuel Hernandez <samdavid2000@hotmail.com>
2021-12-21 01:53:29 -0400

    GUI Changes among other things

--- Biodiversity/Biodiversity/AppShell.xaml.cs
+++ Biodiversity/Biodiversity/AppShell.xaml.cs
@@ -9,10 +9,5 @@ namespace Biodiversity
         {
             InitializeComponent();
         }
-
-        private async void OnMenuItemClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//LoginPage");
-        }
     }
 }
